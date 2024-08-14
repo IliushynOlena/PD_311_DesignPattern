bf42cf9f374849ae80bfa72139bd6882be4af3c8
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-08-14 18:08:34 +0300

    Prototype, Adapter

--- 05_Prototype/Program.cs
+++ 05_Prototype/Program.cs
@@ -1,28 +1,42 @@
 ﻿namespace _05_Prototype
 {
+    interface Prototype<T>
+    {
+        public T Clone();
+    }
     public class IdInfo
     {
-        public int IdNumber;
+        public int IdNumber { get; set; }
 
         public IdInfo(int idNumber)
         {
             this.IdNumber = idNumber;
         }
     }
-    public class Person
+    public class Person : Prototype<Person> 
     {
-        public int Age;
-        public DateTime BirthDate;
-        public string Name;
-        public IdInfo IdInfo;
+        public int Age { get; set; }//value 15 = 15
+        public DateTime BirthDate { get; set; }//value 15.01.2000 = 15.01.2000
+        public string Name { get; set; }//ref 0258855 = 0258855
+        public IdInfo IdInfo { get; set; }// ref 028965 = 028965
 
-        public Person ShallowCopy()
-        {
-            return (Person)this.MemberwiseClone();
-        }
+        //public Person ShallowCopy()
+        //{
+        //    return (Person)this.MemberwiseClone();
+        //}
 
-        public Person DeepCopy()
+        //public Person DeepCopy()
+        //{
+        //    Person clone = (Person)this.MemberwiseClone();
+        //    clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+        //    clone.Name = String.Copy(Name);
+        //    return clone;
+        //}
+
+        public Person Clone()
         {
+            //return (Person)this.MemberwiseClone();
+
             Person clone = (Person)this.MemberwiseClone();
             clone.IdInfo = new IdInfo(IdInfo.IdNumber);
             clone.Name = String.Copy(Name);
@@ -43,9 +57,14 @@
             p1.IdInfo = new IdInfo(777);
 
             // Perform a shallow copy of p1 and assign it to p2.
-            Person p2 = p1.ShallowCopy();
+            //Person p2 = p1.ShallowCopy();
             // Make a deep copy of p1 and assign it to p3.
-            Person p3 = p1.DeepCopy();
+            //Person p3 = p1.DeepCopy();
+
+           
+            Person p2 = p1.Clone();
+            // Make a deep copy of p1 and assign it to p3.
+            Person p3 = p1.Clone();
 
             // Display values of p1, p2 and p3.
             Console.WriteLine("Original values of p1, p2, p3:");
